14569b41d845625787477571dce6198684907aaa
jackajackalop <yixinh@andrew.cmu.edu>
2019-09-12 19:07:31 -0400

    oops maybe should install vstools

--- dirty_hack.bat
+++ dirty_hack.bat
@@ -1,5 +1,4 @@
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" -test
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 @set JAM_TOOLSET=VISUALC
 @set PATH=%PATH%;%~dp0
 @cd %~dp0/../../../Build-Testing
