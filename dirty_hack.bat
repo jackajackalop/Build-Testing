957608acd907e9ff81aca3b27e3e40c66fd93dd1
jackajackalop <yixinh@andrew.cmu.edu>
2019-09-12 17:42:44 -0400

    setting jam_toolest before opening the cmd window..???

--- dirty_hack.bat
+++ dirty_hack.bat
@@ -1,7 +1,7 @@
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-@export JAM_TOOLSET=VISUALC
+@set JAM_TOOLSET=VISUALC
 @set PATH=%PATH%;%~dp0
 @cd %~dp0/../../../Build-Testing
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 @jam
 @exit
 
