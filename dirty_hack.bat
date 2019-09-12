b124a28645f1235e84eed7a571e7195f48900f1d
jackajackalop <yixinh@andrew.cmu.edu>
2019-09-12 19:29:41 -0400

    desparate hopes

--- dirty_hack.bat
+++ dirty_hack.bat
@@ -1,4 +1,4 @@
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 @set JAM_TOOLSET=VISUALC
 @set PATH=%PATH%;%~dp0
 @cd %~dp0/../../../Build-Testing
