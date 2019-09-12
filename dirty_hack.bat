23ca3c391b87e4a95b5697cfdf2fe6a71cf85b66
jackajackalop <yixinh@andrew.cmu.edu>
2019-09-12 17:34:27 -0400

    export instead of set????

--- dirty_hack.bat
+++ dirty_hack.bat
@@ -1,5 +1,5 @@
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-@set JAM_TOOLSET=VISUALC
+@export JAM_TOOLSET=VISUALC
 @set PATH=%PATH%;%~dp0
 @cd %~dp0/../../../Build-Testing
 @jam
