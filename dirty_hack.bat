8efda5f132e8220715c6ee6260660ed4e3fb355a
jackajackalop <yixinh@andrew.cmu.edu>
2019-09-12 18:27:52 -0400

    praying this googled solution works

--- dirty_hack.bat
+++ dirty_hack.bat
@@ -1,7 +1,8 @@
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" -host_arch=amd64 -arch=amd64
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" -test
 @set JAM_TOOLSET=VISUALC
 @set PATH=%PATH%;%~dp0
 @cd %~dp0/../../../Build-Testing
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 @jam
 @exit
 
