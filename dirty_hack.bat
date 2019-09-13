da0eff39e855dfb7bad35c5e4c91d99365971751
jackajackalop <yixinh@andrew.cmu.edu>
2019-09-12 20:00:11 -0400

    imitating jims thing but shrug its basically the same

--- dirty_hack.bat
+++ dirty_hack.bat
@@ -1,6 +1,7 @@
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+@echo Setting JAM_TOOLSET and adding %~dp0 to PATH
 @set JAM_TOOLSET=VISUALC
 @set PATH=%PATH%;%~dp0
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 @cd %~dp0/../../../Build-Testing
 @jam
 @exit
